dfea4225d57a49919557bd80cea7b8b0cc69b105
Courtnee M <courtneemeaghan@gmail.com>
2019-07-21 23:44:28 -0500

    Added delete funtionality and limit to 2 decimal spaces.

--- script.js
+++ script.js
@@ -23,19 +23,21 @@ function operate(operator, a, b) {
       return calculator.add(a, b);
     case "-":
       return calculator.subtract(a, b);
-    case "X":
+    case "x":
        return calculator.multiply(a, b);
     case "/":
        return calculator.divide(a, b);
   }
 }
 
-var num1 = "";
-var num2 = "";
-var operator = "";
-var decimalBtn = document.querySelector("#decimal-btn");
+
 
 function displayNumbers() {
+  let num1 = "";
+  let num2 = "";
+  let operator = "";
+  let decimalBtn = document.querySelector("#decimal-btn");
+
   calcBtns.addEventListener('click', function(e) {
     //debugger;
     if(e.target.className.includes("btn-num")) {
@@ -46,16 +48,19 @@ function displayNumbers() {
         num2 += e.target.textContent;
         display.textContent = `${num1} ${operator} ${num2}`;
       }
-      if(num1.includes(decimalBtn.textContent) && operator === "") {
+      if(String(num1).includes(decimalBtn.textContent) && operator === "") {
         decimalBtn.disabled = true;
-      } else if (num2.includes(decimalBtn.textContent)) {
+      } else if (String(num2).includes(decimalBtn.textContent)) {
         decimalBtn.disabled = true;
       }
     } 
 
     if(e.target.className.includes("btn-op")) {
       if(num1 != "" && num2 != "") {
-        num1 = String(operate(operator, Number(num1), Number(num2)));
+        num1 = operate(operator, Number(num1), Number(num2));
+        if(String(num1).includes(".")) {
+          num1 = num1.toFixed(2)
+        }
         if(num1 == "Infinity") {
           display.textContent = "Hey! You can't do that!"
           num1 = "";
@@ -64,8 +69,8 @@ function displayNumbers() {
         } else {
           num2 = "";
           operator = e.target.textContent;
-          num1 = Number(num1);
-          display.textContent = `${num1.toFixed(2)} ${operator}`;
+          num1 = num1;
+          display.textContent = `${num1} ${operator}`;
         }
       } else if(num1 != "") {
         operator = e.target.textContent;
@@ -73,30 +78,59 @@ function displayNumbers() {
       }
       decimalBtn.disabled = false;
     } 
-    
+
     if (e.target.textContent === "=") {
       if(num1 === "" || operator === "" || num2 === "" ) {
         display.textContent = "error";
       } else {
         console.log(operate(operator, Number(num1), Number(num2)));
         num1 = operate(operator, Number(num1), Number(num2));
+        if(String(num1).includes(".")) {
+          num1 = num1.toFixed(2)
+        }
         if(num1 == "Infinity") {
           display.textContent = "Hey! You can't do that!"
         } else {
-          display.textContent = num1.toFixed(2);
+          display.textContent = num1;
           num1 = "";
           num2 = "";
           operator = "";
         }
       }
       decimalBtn.disabled = false;
-    } else if(e.target.textContent === "C") {
+    } 
+    
+    if(e.target.textContent === "C") {
       display.textContent = "0"
       num1 = "";
       num2 = "";
       operator = "";
       decimalBtn.disabled = false;
     }
+
+    if(e.target.textContent === "DEL") {
+      if(operator === "") {
+        num1 = num1.split("");
+        num1.pop();
+        num1 = num1.join("");
+        display.textContent = `${num1}`;
+        console.log(num1);
+      }
+      if(num1 != "" && operator != "" && num2 === "") {
+        operator = "";
+        display.textContent = `${num1}`
+      }
+      if(operator != "") {
+        num2 = num2.split("");
+        num2.pop();
+        num2 = num2.join("");
+        display.textContent = `${num1} ${operator} ${num2}`;
+      }
+      if(display.textContent === "") {
+        display.textContent = "0";
+      }
+    }
+    
     console.log(num1, operator, num2);
   })
 }
